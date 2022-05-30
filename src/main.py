95713e53ee69c329c71beaada8c38a45288b2918
espstan <espstan@gmail.com>
2022-05-31 01:31:54 +0300

    Add basic singleton implementation

--- src/main.py
+++ src/main.py
@@ -4,6 +4,17 @@ from view import MinesweeperView
 
 
 class Game:
+    __instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls.__instance is None:
+            cls.__instance = super().__new__(cls)
+
+        return cls.__instance
+
+    def __del__(self):
+        self.__class__.__instance = None
+
     def __init__(self):
         self.model = MinesweeperModel()
         self.controller = MinesweeperController(self.model)
